9f81f21bde729ca1c7fb2ed1a07e9ffc4d4ecb6a
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-02-03 16:39:10 +0800

    add new keys

--- configs/key.ts
+++ configs/key.ts
@@ -4,15 +4,32 @@ export const okxAcc1 = {
     secret: process.env.OKX_SECRET_01,
     passphrase: process.env.OKX_PASSPHRASE_01
 }
+
+export const bybitAcc1 = {
+    key: process.env.BYBIT_APIKEY,
+    secret: process.env.BYBIT_SECRET
+}
+
 // Official Account for Holders
 export const okxAcc2 = {
     key: process.env.OKX_APIKEY_02,
     secret: process.env.OKX_SECRET_02,
     passphrase: process.env.OKX_PASSPHRASE_02
 }
+
+export const bybitAcc2 = {
+    key: process.env.BYBITNO2_API,
+    secret: process.env.BYBITNO2_SECRET
+}
+
 // Official Account for Non-holders
 export const okxAcc3 = {
     key: process.env.OKX_APIKEY_03,
     secret: process.env.OKX_SECRET_03,
     passphrase: process.env.OKX_PASSPHRASE_03
+}
+
+export const bybitAcc3 = {
+    key: process.env.BYBITNO3_API,
+    secret: process.env.BYBITNO3_SECRET
 }
