6b67f7bf08f9634dc7671fdfc4c9184927fc8864
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-02-03 15:38:11 +0800

    add keys

---
+++ configs/key.ts
@@ -0,0 +1,18 @@
+// Test Account
+export const okxAcc1 = {
+    key: process.env.OKX_APIKEY_01,
+    secret: process.env.OKX_SECRET_01,
+    passphrase: process.env.OKX_PASSPHRASE_01
+}
+// Official Account for Holders
+export const okxAcc2 = {
+    key: process.env.OKX_APIKEY_02,
+    secret: process.env.OKX_SECRET_02,
+    passphrase: process.env.OKX_PASSPHRASE_02
+}
+// Official Account for Non-holders
+export const okxAcc3 = {
+    key: process.env.OKX_APIKEY_03,
+    secret: process.env.OKX_SECRET_03,
+    passphrase: process.env.OKX_PASSPHRASE_03
+}
